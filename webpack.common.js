2d554a5207e17507724d18ab7e25c4fffacd27dd
JEB <jebarrocal@gmail.com>
2024-05-13 23:03:43 -0300

    - adds ngrok to package - updates ngrok version config file - updates readme - upgrades dependencies versions - enforces node version - updates ts config

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 module.exports = (env) => {
 
-  const path = require("path");
+  const path = require('path');
   const HtmlWebpackPlugin = require('html-webpack-plugin');
 
   function _typescript() {
@@ -13,39 +13,73 @@ module.exports = (env) => {
 
   function _assets() {
     return {
-      test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|mp3|mp4|json|txt)$/i,
-      type: "asset/resource",
+      test: /\.(json|txt)$/i,
+      type: 'asset',
+    };
+  }
+
+  function _fonts() {
+    return {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset',
+      generator: {
+        filename: 'assets/fonts/[name][hash][ext]'
+      },
+    };
+  }
+
+  function _media() {
+    return {
+      test: /\.(mp3|mp4)$/i,
+      type: 'asset',
+      generator: {
+        filename: 'assets/media/[name][hash][ext]'
+      },
+    };
+  }
+
+  function _imgs() {
+    return {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset',
+      generator: {
+        filename: 'assets/imgs/[name][hash][ext]'
+      },
     };
   }
 
   function _scss() {
     return {
-      test: /\.(scss)$/,
+      test: /\.s[ac]ss$/i,
       use: [{
-          // Adds CSS to the DOM by injecting a `<style>` tag
-          loader: 'style-loader'
-        },
-        {
-          // Interprets `@import` and `url()` like `import/require()` and will resolve them
-          loader: 'css-loader'
-        },
-        {
-          // Loader for webpack to process CSS with PostCSS
-          loader: 'postcss-loader',
-          options: {
-            postcssOptions: {
-              plugins: function () {
-                return [
-                  require('autoprefixer')
-                ];
-              },
+        // Adds CSS to the DOM by injecting a `<style>` tag
+        loader: 'style-loader'
+      },
+      {
+        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+        loader: 'css-loader'
+      },
+      {
+        // Loader for webpack to process CSS with PostCSS
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            plugins: function () {
+              return [
+                require('autoprefixer')
+              ];
             },
-          }
-        },
-        {
-          // Loads a SASS/SCSS file and compiles it to CSS
-          loader: 'sass-loader'
+          },
         }
+      },
+      {
+        // Loads a SASS/SCSS file and compiles it to CSS
+        loader: 'sass-loader',
+        options: {
+          // Prefer `dart-sass`
+          implementation: require('sass'),
+        },
+      }
       ]
     };
   }
@@ -53,7 +87,10 @@ module.exports = (env) => {
   function _ejs() {
     return {
       test: /\.ejs$/i,
-      use: ['html-loader', 'template-ejs-loader'],
+      use: [
+        'html-loader',
+        'template-ejs-loader',
+      ],
     };
   }
 
@@ -63,6 +100,9 @@ module.exports = (env) => {
       _scss(),
       _typescript(),
       _assets(),
+      _fonts(),
+      _media(),
+      _imgs(),
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
     ];
@@ -71,8 +111,8 @@ module.exports = (env) => {
   function _htmlWebpackPlugin() {
     return [
       new HtmlWebpackPlugin({
-        template: "./src/index.ejs",
-        filename: "./index.html",
+        template: './src/index.ejs',
+        filename: './index.html',
         cache: false,
         inject: 'body',
         minify: true,
@@ -90,11 +130,11 @@ module.exports = (env) => {
   function _config(env) {
     return {
       target: 'web',
-      entry: "./src/app.ts",
+      entry: './src/app.ts',
       output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
-        assetModuleFilename: 'assets/[name][ext]'
+        assetModuleFilename: 'assets/[name][hash][ext]'
       },
       plugins: _plugins(env),
       module: {
